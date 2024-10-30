12a81857239159d30cee89835712b05976a070d8
Susan Hooks <susanhooks@gmail.com>
2024-10-30 12:46:01 -0600

    WIP

--- jobs/csv_locations.py
+++ jobs/csv_locations.py
@@ -4,7 +4,69 @@ from nautobot.core.jobs import Job, BooleanVar, FileVar, ObjectVar
 from nautobot.dcim.models import Location, LocationType
 from nautobot.extras.models import Status
 
-from state_abbreviations import us_state_abbreviations
+
+STATE_ABBREVIATIONS = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AS": "American Samoa",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "DC": "District of Columbia",
+    "FM": "Federated States of Micronesia",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "GU": "Guam",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MH": "Marshall Islands",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "MP": "Northern Mariana Islands",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PW": "Palau",
+    "PA": "Pennsylvania",
+    "PR": "Puerto Rico",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    "VI": "Virgin Islands",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming",
+
+}
 
 name = "CSV Locations"
 
@@ -16,7 +78,6 @@ class CSVImportJob(Job):
             "created": 0,
             "errors": 0,
         }
-        self.us_state_abbreviations = us_state_abbreviations
 
 
     class Meta:
@@ -60,8 +121,8 @@ class CSVImportJob(Job):
 
             # convert state name to full name
 
-            if self.us_state_abbreviations.get(state_name):
-                state_name = self.us_state_abbreviations[state_name]
+            if STATE_ABBREVIATIONS.get(state_name):
+                state_name = STATE_ABBREVIATIONS[state_name]
 
             state, _ = Location.objects.get_or_create(
                 name=state_name,
