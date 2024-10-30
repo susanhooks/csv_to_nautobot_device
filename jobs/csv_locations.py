0bfe03496908f78d058b46c0b994af341f5ab099
Susan Hooks <susanhooks@gmail.com>
2024-10-30 12:42:56 -0600

    rest of job

--- jobs/csv_locations.py
+++ jobs/csv_locations.py
@@ -1,6 +1,10 @@
+import csv 
+
 from nautobot.core.jobs import Job, BooleanVar, FileVar, ObjectVar
 from nautobot.dcim.models import Location, LocationType
 from nautobot.extras.models import Status
+
+from state_abbreviations import us_state_abbreviations
 
 name = "CSV Locations"
 
@@ -12,6 +16,7 @@ class CSVImportJob(Job):
             "created": 0,
             "errors": 0,
         }
+        self.us_state_abbreviations = us_state_abbreviations
 
 
     class Meta:
@@ -27,9 +32,57 @@ class CSVImportJob(Job):
     location_status = ObjectVar(model=Status, query_params={"content_types": "dcim.location"})
 
     def parse_site_type(self, site_name):
-        pass
+        site_type_map = {
+            "DC": "Data Center",
+            "BR": "Branch",
+        }
+
+        split_name = site_name.split("-")
+
+        site_type_abbr = split_name[1] if len(split_name) > 1 else ""
+
+        return site_type_map.get(site_type_abbr, "Data Center")
 
     def run(self, csv_file, debug, location_status, *args, **kwargs):
         """Run the job."""
-        pass
+        self.debug = debug
+        self.csv_file = csv_file
+        self.location_status = location_status
+        
+        csv_reader = csv.DictReader(csv_file)
+        row_count = 1
+
+        for row in csv_reader:
+            site_name = row.get("name", "")
+            state_name = row.get("state", "")
+            city_name = row.get("city", "")
+            site_type = self.parse_site_type(site_name)
+
+            # convert state name to full name
+
+            if self.us_state_abbreviations.get(state_name):
+                state_name = self.us_state_abbreviations[state_name]
+
+            state, _ = Location.objects.get_or_create(
+                name=state_name,
+                status=self.location_status,
+                type=LocationType.objects.get(name="State"),
+            )
+            city, _ = Location.objects.get_or_create(
+                name=city_name,
+                status=self.location_status,
+                type=LocationType.objects.get(name="City"),
+                parent=state,
+            )
+            site, _ = Location.objects.get_or_create(
+                name=site_name,
+                status=self.location_status,
+                type=LocationType.objects.get(name=site_type),
+                parent=city,
+            )
+            row_count += 1
+            self.job_results["created"] += 1
+        return self.job_results
+
+
         
